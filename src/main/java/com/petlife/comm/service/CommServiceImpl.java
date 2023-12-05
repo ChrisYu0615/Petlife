99bdb102be60f829b0d8ed323ecb2a39f06f25b1
harvie hung <harviehung@gmail.com>
2023-12-05 14:33:52 +0800

    add: CommServiceImlp 加了點開頭

--- src/main/java/com/petlife/comm/service/CommServiceImpl.java
+++ src/main/java/com/petlife/comm/service/CommServiceImpl.java
@@ -1,7 +1,14 @@
 package com.petlife.comm.service;
 
 import com.google.protobuf.Service;
+import com.petlife.comm.dao.CommDAO;
+import com.petlife.comm.dao.CommDAOImpl;
+import com.petlife.test.TestCommDAO;
 
 public class CommServiceImpl implements Service{
+	private CommDAO dao;
 	
+	public  CommServiceImpl() {
+		dao = new CommDAOImpl();
+	}
 }
