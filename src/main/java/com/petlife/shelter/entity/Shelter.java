70c39f738dd04f6d94e78004a9beabb779c4fc12
闕思涵 <dammy821210@gamil.com>
2023-12-07 17:07:15 +0800

    shelter dao

---
+++ src/main/java/com/petlife/shelter/entity/Shelter.java
@@ -0,0 +1,305 @@
+package com.petlife.shelter.entity;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Table;
+
+import com.petlife.admin.entity.AcctState;
+import com.petlife.admin.entity.AcctType;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+@Table(name = "shelter")
+public class Shelter {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "shelter_id", updatable = false)
+	private Integer shelterId;
+	
+	@Column(name = "shelter_acct")
+	private String shelterAcct;
+	
+	@Column(name = "shelter_pwd")
+	private String shelterPwd;
+	
+	@Column(name = "shelter_name")
+	private String shelterName;
+
+	@Column(name = "shelter_create_time", insertable = false)	
+	private Timestamp shelterCreateTime;
+	
+	@Column(name = "shelter_pwd_err_times",columnDefinition="tinyint", insertable = false)
+	private Integer shelterPwdErrTimes;
+	
+	@Column(name = "shelter_phone_num")
+	private String shelterPhoneNum;
+	
+	@Column(name = "shelter_address")
+	private String shelterAddress;
+	
+	@Column(name = "shelter_photo",columnDefinition="longblob")
+	private byte[] shelterPhoto;
+	
+	@Column(name = "shelter_introduction",columnDefinition="longtext")
+	private String shelterIntroduction;
+	
+	@ManyToOne
+	@JoinColumn(name = "acct_type_id", referencedColumnName = "acct_type_id", updatable = false, insertable = false)
+	private AcctType acctType;
+	
+	@Column(name = "shelter_lng")
+	private Double shelterLng;
+	
+	
+	@Column(name = "shelter_lat")
+	private Double shelterLat;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "acct_state_id", referencedColumnName = "acct_state_id",updatable = false, insertable = false)
+	private AcctState acctState;
+	
+	
+	public Shelter(String shelterAcct, String shelterPwd, String shelterName, String shelterPhoneNum, 
+			String shelterAddress, String shelterIntroduction, Double shelterLng, Double shelterLat) {
+		super();
+		this.shelterAcct = shelterAcct;
+		this.shelterPwd = shelterPwd;
+		this.shelterName = shelterName;
+		this.shelterPhoneNum = shelterPhoneNum;
+		this.shelterAddress = shelterAddress;
+		this.shelterIntroduction = shelterIntroduction;
+		this.shelterLng = shelterLng;
+		this.shelterLat = shelterLat;
+	}
+	
+	
+
+	public Shelter(Integer shelterId, String shelterAcct, String shelterPwd, String shelterName,
+			Timestamp shelterCreateTime, Integer shelterPwdErrTimes, String shelterPhoneNum, String shelterAddress,
+			byte[] shelterPhoto, String shelterIntroduction, AcctType acctType, Double shelterLng, Double shelterLat,
+			AcctState acctState) {
+		super();
+		this.shelterId = shelterId;
+		this.shelterAcct = shelterAcct;
+		this.shelterPwd = shelterPwd;
+		this.shelterName = shelterName;
+		this.shelterCreateTime = shelterCreateTime;
+		this.shelterPwdErrTimes = shelterPwdErrTimes;
+		this.shelterPhoneNum = shelterPhoneNum;
+		this.shelterAddress = shelterAddress;
+		this.shelterPhoto = shelterPhoto;
+		this.shelterIntroduction = shelterIntroduction;
+		this.acctType = acctType;
+		this.shelterLng = shelterLng;
+		this.shelterLat = shelterLat;
+		this.acctState = acctState;
+	}
+
+
+
+	public Shelter() {
+		
+	}
+
+
+
+	public Integer getShelterId() {
+		return shelterId;
+	}
+
+
+
+	public void setShelterId(Integer shelterId) {
+		this.shelterId = shelterId;
+	}
+
+
+
+	public String getShelterAcct() {
+		return shelterAcct;
+	}
+
+
+
+	public void setShelterAcct(String shelterAcct) {
+		this.shelterAcct = shelterAcct;
+	}
+
+
+
+	public String getShelterPwd() {
+		return shelterPwd;
+	}
+
+
+
+	public void setShelterPwd(String shelterPwd) {
+		this.shelterPwd = shelterPwd;
+	}
+
+
+
+	public String getShelterName() {
+		return shelterName;
+	}
+
+
+
+	public void setShelterName(String shelterName) {
+		this.shelterName = shelterName;
+	}
+
+
+
+	public Timestamp getShelterCreateTime() {
+		return shelterCreateTime;
+	}
+
+
+
+	public void setShelterCreateTime(Timestamp shelterCreateTime) {
+		this.shelterCreateTime = shelterCreateTime;
+	}
+
+
+
+	public Integer getShelterPwdErrTimes() {
+		return shelterPwdErrTimes;
+	}
+
+
+
+	public void setShelterPwdErrTimes(Integer shelterPwdErrTimes) {
+		this.shelterPwdErrTimes = shelterPwdErrTimes;
+	}
+
+
+
+	public String getShelterPhoneNum() {
+		return shelterPhoneNum;
+	}
+
+
+
+	public void setShelterPhoneNum(String shelterPhoneNum) {
+		this.shelterPhoneNum = shelterPhoneNum;
+	}
+
+
+
+	public String getShelterAddress() {
+		return shelterAddress;
+	}
+
+
+
+	public void setShelterAddress(String shelterAddress) {
+		this.shelterAddress = shelterAddress;
+	}
+
+
+
+	public byte[] getShelterPhoto() {
+		return shelterPhoto;
+	}
+
+
+
+	public void setShelterPhoto(byte[] shelterPhoto) {
+		this.shelterPhoto = shelterPhoto;
+	}
+
+
+
+	public String getShelterIntroduction() {
+		return shelterIntroduction;
+	}
+
+
+
+	public void setShelterIntroduction(String shelterIntroduction) {
+		this.shelterIntroduction = shelterIntroduction;
+	}
+
+
+
+	public AcctType getAcctType() {
+		return acctType;
+	}
+
+
+
+	public void setAcctType(AcctType acctType) {
+		this.acctType = acctType;
+	}
+
+
+
+	public Double getShelterLng() {
+		return shelterLng;
+	}
+
+
+
+	public void setShelterLng(Double shelterLng) {
+		this.shelterLng = shelterLng;
+	}
+
+
+
+	public Double getShelterLat() {
+		return shelterLat;
+	}
+
+
+
+	public void setShelterLat(Double shelterLat) {
+		this.shelterLat = shelterLat;
+	}
+
+
+
+	public AcctState getAcctState() {
+		return acctState;
+	}
+
+
+
+	public void setAcctState(AcctState acctState) {
+		this.acctState = acctState;
+	}
+
+
+
+	// 搜尋時不讓收容所資料重複，要加上hashcode跟equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(shelterId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Shelter other = (Shelter) obj;
+		return Objects.equals(shelterId, other.shelterId);
+	}
+
+	
+
+}
