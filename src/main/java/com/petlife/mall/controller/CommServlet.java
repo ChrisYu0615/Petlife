697d9db4572f3cf831e16b3b49a9c73c7f673363
Chia Jen <abc0956000813@gmail.com>
2023-12-18 17:32:07 +0800

    完成Comm，交接漢宇修復修改功能，以及套前端

---
+++ src/main/java/com/petlife/mall/controller/CommServlet.java
@@ -0,0 +1,428 @@
+package com.petlife.mall.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.petlife.mall.dao.CommDAO;
+import com.petlife.mall.dao.impl.CommDAOImpl;
+import com.petlife.mall.entity.Comm;
+import com.petlife.mall.entity.CommCat;
+import com.petlife.mall.service.CommService;
+import com.petlife.mall.service.impl.CommServiceImpl;
+import com.petlife.seller.entity.Seller;
+
+@WebServlet("/comm/comm.do")
+@MultipartConfig
+public class CommServlet extends HttpServlet {
+	// 一個 servlet 實體對應一個 service 實體
+	private CommService commService;
+
+	@Override
+	public void init() throws ServletException {
+		commService = new CommServiceImpl();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		String forwardPath = "";
+
+		switch (action) {
+		case "getOne_For_Display":
+			// 來自selact_page.jsp
+			forwardPath = getOneDisplay(req, res);
+			break;
+		case "getOne_For_Update":
+			// 來自listAllComm.jsp
+			forwardPath = getOneUpdate(req, res);
+			break;
+		case "update":
+			// 來自update_comm_input.jsp
+			forwardPath = update(req, res);
+			break;
+		case "insert":
+			// 來自listAllComm.jsp
+			forwardPath = insert(req, res);
+			break;
+		case "delete":
+			// 來自listAllComm.jsp
+			forwardPath = delete(req, res);
+			break; // 新加的break
+		default:
+			forwardPath = "/comm/select_page.jsp";
+		}
+
+		res.setContentType("text/html; charset=UTF-8");
+		RequestDispatcher dispatcher = req.getRequestDispatcher(forwardPath);
+		dispatcher.forward(req, res);
+	}
+
+	// 1,查詢
+	private String getOneDisplay(HttpServletRequest req, HttpServletResponse res) {
+		// 錯誤處理
+		List<String> errorMsgs = new ArrayList<>();
+		req.setAttribute("errorMsgs", errorMsgs);
+
+///*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+		String str = req.getParameter("commId");
+
+		if (str == null || (str.trim()).length() == 0) {
+			errorMsgs.add("請輸入商品編號");
+		}
+		// Send the use back to the form, if there were errors
+		if (!errorMsgs.isEmpty()) {
+			return "/comm/select_page.jsp";// 程式中斷
+		}
+
+		Integer commId = null;
+		try {
+			commId = Integer.valueOf(str);
+		} catch (Exception e) {
+			errorMsgs.add("商品編號格式不正確");
+		}
+		// Send the use back to the form, if there were errors
+		if (!errorMsgs.isEmpty()) {
+			return "/comm/select_page.jsp";// 程式中斷
+		}
+
+///*************************** 2.開始查詢資料 *****************************************/
+//		CommService coupoService = new CommService();
+		commService = new CommServiceImpl();
+		Comm comm = commService.findByPk(commId);
+
+		if (comm == null) {
+			errorMsgs.add("查無資料");
+		}
+		// Send the use back to the form, if there were errors
+		if (!errorMsgs.isEmpty()) {
+			return "/comm/select_page.jsp";// 程式中斷
+		}
+
+///*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
+		req.setAttribute("comm", comm); // 資料庫取出的manage物件,存入req
+		return "/comm/listOneComm.jsp"; // 20231128
+	}
+	// ===================================================
+
+	// 2,修改
+	private String getOneUpdate(HttpServletRequest req, HttpServletResponse res) {
+//		System.out.println(req.getParameter("commId"));
+		Integer commId = Integer.valueOf(req.getParameter("commId")); 
+		System.out.println("+++" + commId);
+
+		Comm comm = commService.findByPk(commId);
+		System.out.println(comm);
+
+		req.setAttribute("comm", comm);
+		return "/comm/update_comm_input.jsp";
+	}
+
+	private String update(HttpServletRequest req, HttpServletResponse res) {
+		// 錯誤處理
+		List<String> errorMsgs = new ArrayList<>();
+		req.setAttribute("errorMsgs", errorMsgs);
+
+///*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+		Integer commId = Integer.parseInt(req.getParameter("commId"));
+		Integer sellerId = Integer.parseInt(req.getParameter("seller"));
+		String commName = req.getParameter("commName");
+		String commDesc = req.getParameter("commDesc");
+		Integer commState = Integer.parseInt(req.getParameter("commState"));
+				
+		Timestamp listDatetime = java.sql.Timestamp.valueOf(req.getParameter("listDatetime").trim());
+//		Timestamp sellerEvaluateTime;
+//		try {
+//			listDatetime = java.sql.Timestamp.valueOf(req.getParameter("listDatetime").trim());
+//		} catch (IllegalArgumentException e) {
+//			listDatetime = new java.sql.Timestamp(System.currentTimeMillis());
+//			errorMsgs.add("請輸入時間!");
+//		}
+		
+		byte[] commImg = null;
+	    try {
+	        InputStream in = req.getPart("commImg").getInputStream();
+	        if (in.available() != 0) {
+	        	commImg = new byte[in.available()];
+	            in.read(commImg);
+	            in.close();
+	        } else {
+	            errorMsgs.add("商品圖片: 请上傳照片");
+	        }
+	    } catch (IOException | ServletException e) {
+	        errorMsgs.add("圖片上傳失敗: " + e.getMessage());
+	    }
+		
+	    Integer commCatId = Integer.parseInt(req.getParameter("commCat"));
+	    Integer commStock = Integer.parseInt(req.getParameter("commStock"));
+		
+		
+		
+		BigDecimal commPrice;
+		try {
+			// 假設 req 是 HttpServletRequest 物件
+			String commPriceStr = req.getParameter("commPrice");
+			
+			// 將字串轉換為 BigDecimal
+			commPrice = new BigDecimal(commPriceStr);
+			
+			// 設定默認值，保留兩位小數
+			commPrice = commPrice.setScale(2, RoundingMode.HALF_UP);
+			
+			// 在這裡您可以使用 commPrice 進行後續的操作
+		} catch (NumberFormatException e) {
+			// 處理轉換失敗的情況，例如記錄錯誤或提供默認值
+			e.printStackTrace();
+			// 例如，提供默認值
+			commPrice = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+		}
+		
+		BigDecimal commOnsalePrice;
+		try {
+		    // 假設 req 是 HttpServletRequest 物件
+		    String commOnsalePriceStr = req.getParameter("commOnsalePrice");
+
+		    if (commOnsalePriceStr != null) {
+		        // 將字串轉換為 BigDecimal
+		        commOnsalePrice = new BigDecimal(commOnsalePriceStr);
+
+		        // 設定默認值，保留兩位小數
+		        commOnsalePrice = commOnsalePrice.setScale(2, RoundingMode.HALF_UP);
+
+		        // 在這裡您可以使用 commPrice 進行後續的操作
+		    } else {
+		        // 提供默認值
+		        commOnsalePrice = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+		    }
+		} catch (NumberFormatException e) {
+		    // 處理轉換失敗的情況，例如記錄錯誤或提供默認值
+		    e.printStackTrace();
+		    // 例如，提供默認值
+		    commOnsalePrice = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+		}
+		
+		Integer commViewCount = Integer.parseInt(req.getParameter("commViewCount"));
+		
+
+		
+
+
+//============================================
+
+		Comm comm = new Comm();
+		comm.setCommId(commId);
+
+		Seller seller = new Seller();
+		seller.setSellerId(sellerId);
+		comm.setSeller(seller);
+
+		CommCat commCat = new CommCat();
+		commCat.setCommCatId(commCatId);
+		comm.setCommCat(commCat);
+		
+		comm.setCommName(commName);
+		comm.setCommDesc(commDesc);
+		comm.setCommState(commState);
+		comm.setListDatetime(listDatetime);
+		comm.setCommImg(commImg);
+//		comm.setCommCat(commCat);
+		comm.setCommStock(commStock);
+		comm.setCommPrice(commPrice);
+		comm.setCommOnsalePrice(commOnsalePrice);
+		comm.setCommViewCount(commViewCount);
+
+//==========================================		
+		// Send the use back to the form, if there were errors
+		if (!errorMsgs.isEmpty()) {
+			req.setAttribute("comm", comm); // 含有輸入格式錯誤的manage物件,也存入req
+			return "/comm/update_comm_input.jsp"; // 程式中斷
+		}
+
+///*************************** 2.開始修改資料 *****************************************/
+		commService.update(comm);
+		req.setAttribute("comm", commService.findByPk(commId));
+
+///*************************** 3.修改完成,準備轉交(Send the Success view) *************/
+		req.setAttribute("comm", comm); // 資料庫update成功後,正確的的manage物件,存入req
+		return "/comm/listOneComm.jsp";
+	}
+
+	// ===================================================
+
+	// 3,新增
+	private String insert(HttpServletRequest req, HttpServletResponse res) {
+		// 錯誤處理
+
+		List<String> errorMsgs = new ArrayList<>();
+		req.setAttribute("errorMsgs", errorMsgs);
+//		/*********************** 1.接收請求參數 - 輸入格式的錯誤處理 *************************/
+
+//		Integer commId = Integer.parseInt(req.getParameter("commId"));
+		Integer sellerId = Integer.parseInt(req.getParameter("seller"));
+		String commName = req.getParameter("commName");
+		String commDesc = req.getParameter("commDesc");
+		Integer commState = Integer.parseInt(req.getParameter("commState"));
+				
+		Timestamp listDatetime = java.sql.Timestamp.valueOf(req.getParameter("listDatetime").trim());
+//		Timestamp sellerEvaluateTime;
+//		try {
+//			listDatetime = java.sql.Timestamp.valueOf(req.getParameter("listDatetime").trim());
+//		} catch (IllegalArgumentException e) {
+//			listDatetime = new java.sql.Timestamp(System.currentTimeMillis());
+//			errorMsgs.add("請輸入時間!");
+//		}
+		
+		byte[] commImg = null;
+	    try {
+	        InputStream in = req.getPart("commImg").getInputStream();
+	        if (in.available() != 0) {
+	        	commImg = new byte[in.available()];
+	            in.read(commImg);
+	            in.close();
+	        } else {
+	            errorMsgs.add("商品圖片: 请上傳照片");
+	        }
+	    } catch (IOException | ServletException e) {
+	        errorMsgs.add("圖片上傳失敗: " + e.getMessage());
+	    }
+		
+	    Integer commCatId = Integer.parseInt(req.getParameter("commCat"));
+	    Integer commStock = Integer.parseInt(req.getParameter("commStock"));
+		
+		
+		
+		BigDecimal commPrice;
+		try {
+			// 假設 req 是 HttpServletRequest 物件
+			String commPriceStr = req.getParameter("commPrice");
+			
+			// 將字串轉換為 BigDecimal
+			commPrice = new BigDecimal(commPriceStr);
+			
+			// 設定默認值，保留兩位小數
+			commPrice = commPrice.setScale(2, RoundingMode.HALF_UP);
+			
+			// 在這裡您可以使用 commPrice 進行後續的操作
+		} catch (NumberFormatException e) {
+			// 處理轉換失敗的情況，例如記錄錯誤或提供默認值
+			e.printStackTrace();
+			// 例如，提供默認值
+			commPrice = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+		}
+		
+		BigDecimal commOnsalePrice;
+		try {
+		    // 假設 req 是 HttpServletRequest 物件
+		    String commOnsalePriceStr = req.getParameter("commOnsalePrice");
+
+		    if (commOnsalePriceStr != null) {
+		        // 將字串轉換為 BigDecimal
+		        commOnsalePrice = new BigDecimal(commOnsalePriceStr);
+
+		        // 設定默認值，保留兩位小數
+		        commOnsalePrice = commOnsalePrice.setScale(2, RoundingMode.HALF_UP);
+
+		        // 在這裡您可以使用 commPrice 進行後續的操作
+		    } else {
+		        // 提供默認值
+		        commOnsalePrice = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+		    }
+		} catch (NumberFormatException e) {
+		    // 處理轉換失敗的情況，例如記錄錯誤或提供默認值
+		    e.printStackTrace();
+		    // 例如，提供默認值
+		    commOnsalePrice = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+		}
+		
+		Integer commViewCount = Integer.parseInt(req.getParameter("commViewCount"));
+		
+
+		// ============================================
+
+		Comm comm = new Comm();
+//		comm.setCommId(commId);
+
+		Seller seller = new Seller();
+		seller.setSellerId(sellerId);
+		comm.setSeller(seller);
+
+		CommCat commCat = new CommCat();
+		commCat.setCommCatId(commCatId);
+		comm.setCommCat(commCat);
+		
+		comm.setCommName(commName);
+		comm.setCommDesc(commDesc);
+		comm.setCommState(commState);
+		comm.setListDatetime(listDatetime);
+		comm.setCommImg(commImg);
+//		comm.setCommCat(commCat);
+		comm.setCommStock(commStock);
+		comm.setCommPrice(commPrice);
+		comm.setCommOnsalePrice(commOnsalePrice);
+		comm.setCommViewCount(commViewCount);
+
+		// ==========================================
+
+//		/*************************** 2.開始新增資料 ***************************************/
+
+		try {
+			System.out.println("新增成功");
+			commService.add(comm);
+		} catch (Exception e) {
+			System.out.println("新增失敗" + e.getMessage());
+			e.getMessage();
+		}
+		;
+
+//		if (comm.getCommId() != null && comm.getCommId() > 0) {
+//			System.out.println("新增成功2");
+//		} else {
+//			System.out.println("新增失敗2");
+//		}
+
+//		/*************************** 3.新增完成,準備轉交(Send the Success view) ***********/
+		return "/comm/select_page.jsp";
+	}
+
+	// ===================================================
+
+	// 4,刪除
+	private String delete(HttpServletRequest req, HttpServletResponse res) {
+//		  List<String> errorMsgs = new List<String>();
+		// Store this set in the request scope, in case we need to
+		// send the ErrorPage view.
+//          req.setAttribute("errorMsgs", errorMsgs);
+
+//          /**1.接收請求參數**/
+		Integer commId = Integer.valueOf(req.getParameter("commId"));
+
+//          /**2.開始刪除資料**/
+		CommDAO commDAO = new CommDAOImpl();
+		commDAO.delete(commId);
+//          /**3.刪除完成,準備轉交(Send the Success view)**/
+		return "/comm/listAllComm.jsp";
+//          String url = "/comm/listAllComm.jsp";
+//          RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
+//          successView.forward(req, res);
+	}
+
+}
