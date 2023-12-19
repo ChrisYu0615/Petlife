4fa185e40b5e1e3c97e8202d9193fd100dbe3e7e
harvie hung <harviehung@gmail.com>
2023-12-19 17:19:44 +0800

    feat: add cart DAO

---
+++ src/main/java/com/petlife/mall/dao/CartDAO.java
@@ -0,0 +1,19 @@
+package com.petlife.mall.dao;
+
+import java.util.List;
+
+import com.petlife.mall.entity.Cart;
+
+public interface CartDAO {
+	//增
+	public Integer add(Cart cart);
+	//刪
+	public Integer delete(Integer cartId);
+	//改
+	public Integer update(Cart cart);
+	//查(單個)
+	public Cart findByPK(Integer cartId);
+	//查(多個)
+	public List<Cart> getAll();
+	public List<Cart> getAllByUserId();
+}
